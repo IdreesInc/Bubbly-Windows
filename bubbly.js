26a7e0b8a744b9d9f48f54d9e26ae724a1f5a495
Idrees Hassan <idrees@idreesinc.com>
2021-09-04 12:27:30 -0400

    Perfect bubble behavior with different element heights

--- bubbly.js
+++ bubbly.js
@@ -1,19 +1,25 @@
 const SPACE = 20;
 const BUBBLE_DIAMETER = 75;
+const TOTAL_TIME = 3;
 
 let windows = document.getElementsByClassName("window");
 let bubbles = [];
+let distances = [];
 
-let y = 0;
+let y = -SPACE;
 for (let i = windows.length - 1; i >= 0; i--) {
     let window = windows[i];
+    y += window.offsetHeight + SPACE;
     let bubble = document.createElement("div");
-    y += window.offsetHeight;
     bubble.id = "bubble-" + i;
     bubble.className = "bubble";
     bubble.style.top = y + "px";
-    console.log( bubble.style.top)
     window.append(bubble)
-    gsap.to("#bubble-" + i, {duration: 3 - i, top: -BUBBLE_DIAMETER, ease:Linear.easeNone});
+    distances[i] = y;
 }
 
+for (let i = 0; i < distances.length; i++) {
+    let distance = distances[i];
+    let duration = TOTAL_TIME * ((distance + BUBBLE_DIAMETER) / (y + BUBBLE_DIAMETER));
+    gsap.to("#bubble-" + i, {duration: duration, top: -BUBBLE_DIAMETER, ease:Linear.easeNone});
+}
